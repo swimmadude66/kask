aa12f7b5ff46855580f838029b914223fa623fc9
Matt Gold <mgold@kabbage.com>
2017-05-01 18:15:51 -0400

    Add chalk progress bar

--- src/client/components/dashboard/tap/component.ts
+++ src/client/components/dashboard/tap/component.ts
@@ -15,6 +15,9 @@ export class TapComponent implements OnInit {
     private loaded: boolean;
     private editing: boolean = false;
 
+    //TODO: pull from flow sensors
+    private percentFull: number = Math.random()*100;
+
     @Input() info: Tap;
     @Input() tapNum: number;
     @Output() remove: EventEmitter<number> = new EventEmitter<number>();
