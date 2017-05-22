4f6b2a872124ed7381a6f70ea9fbcd7cd438f583
Matt Gold <mgold@kabbage.com>
2017-05-22 13:58:54 -0400

    remove empty datapoints from keg level chart

--- src/client/components/stats/keg-level/component.ts
+++ src/client/components/stats/keg-level/component.ts
@@ -113,14 +113,13 @@ export class KegsChartComponent implements OnInit, OnChanges {
             };
         }).reduce((result, curr) => {
             let kegId = curr.KegId;
-            for(let kegId in result) {
-                let indexForDate = result[kegId].findIndex(data => data.x.getTime() === curr.Date.getTime());
-                if (indexForDate < 0) {
-                    let prevY = result[kegId][result[kegId].length - 1].y;
-                    result[kegId].push({x: curr.Date, y: prevY});
-                }
-            }
+
             let indexForDate = result[kegId].findIndex(data => data.x.getTime() === curr.Date.getTime());
+            if (indexForDate < 0) {
+                let prevY = result[kegId][result[kegId].length - 1].y;
+                result[kegId].push({x: curr.Date, y: prevY});
+                indexForDate = 0;
+            }
             result[kegId][indexForDate].y -= curr.Volume;
 
             return result;
